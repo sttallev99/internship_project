1ad61ba04ec30231640c7fa27c50f2d616309eef
Георги Сталев <sttallev99@gmail.com>
2024-10-24 22:42:05 +0300

    Create solution for task_03

--- topic two/tasks.js
+++ topic two/tasks.js
@@ -56,9 +56,32 @@ function task02_search(arr) {
         Minimum value is: ${min_elem}
         Maximum value is: ${max_elem}
         Median valie is: ${median_value}
-        `)
+    `)
 }
 
-task02_search([1, 3, 4, 2, 6, 5, 8, 7])
+// searching for an increasing sequence of maximum length in the original array.
+function task03_selection(arr) {
+    longest_seq_lenght = 0;
+    increasing_seq = [];
+
+    for (let num of arr) {
+        if(increasing_seq.length == 0) {
+            increasing_seq.unshift(num);
+        } else {
+            if(num > increasing_seq[0]) {
+                increasing_seq.unshift(num);
+            } else {
+                if (increasing_seq.length > longest_seq_lenght) {
+                    longest_seq_lenght = increasing_seq.length;
+                }
+                increasing_seq = []
+                increasing_seq.unshift(num)
+            }
+        }
+    }
+    console.log(`increasing sequence of maximum length is: ${longest_seq_lenght}`);
+}
+
+task03_selection([1, 3, 7, 4, 6, 7, 8, 1, 2, 5, 7, 8, 90, 1]);
 
 
