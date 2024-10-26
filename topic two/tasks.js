8049330b9c3f4a1d9894a688dceab73b9ee6fd8e
Георги Сталев <sttallev99@gmail.com>
2024-10-26 10:14:23 +0300

    Create solution for task_04

--- topic two/tasks.js
+++ topic two/tasks.js
@@ -82,6 +82,193 @@ function task03_selection(arr) {
     console.log(`increasing sequence of maximum length is: ${longest_seq_lenght}`);
 }
 
-task03_selection([1, 3, 7, 4, 6, 7, 8, 1, 2, 5, 7, 8, 90, 1]);
+function task04_dateFormatter(inp) {
+    class DateParser {
+        #monthNames
+
+        constructor(dateInp, inpFormat) {
+            this.dateInp = dateInp;
+            this.inpFormat = inpFormat;
+            this.date = undefined;
+            this.#monthNames = [
+                'January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                'August', 'September', 'October', 'November', 'December'
+            ]
+
+            const createDate = (year, month, day) => new Date(year, month - 1, day);
+            const validateData = (month, day) => {
+                month = Number(month);
+                day = Number(day);
+                if((month < 1 || month > 11) || (day < 1 || day > 31)) {
+                    return {success: false, message: 'Invalid day or month'}
+                }
+                else {
+                    return {success: true, message: ''}
+                }
+            }
+            const getDateValues = () => {
+                let year = this.date.getFullYear();
+                let month = this.date.getMonth() + 1;
+                let day = this.date.getDate();
+                if (month < 10) {
+                    month = `0${month}`
+                }
+                if (day < 10) {
+                    day = `0${day}`
+                }
+
+                return {year, month, day}
+            }
+            
+            this.dateFormarts = {
+                'YYYYMMDD': () => {
+                    if (this.date) {
+                        const {year, month, day} = getDateValues();
+                        return `${year}${month}${day}`;
+                    } else {
+                        const year = this.dateInp.slice(0, 4);
+                        const month = this.dateInp.slice(4, 6);
+                        const day = this.dateInp.slice(6);
+                        const {success, message} = validateData(month, day);
+                        if(!success) {
+                            console.log(message);
+                            return;
+                        }
+                        this.date = createDate(year, month, day);
+                    }
+                },
+                'YYYY-MM-DD': () => {
+                    if (this.date) {
+                        const {year, month, day} = getDateValues();
+                        return `${year}-${month}-${day}`
+                    } else {
+                        const [year, month, day] = this.dateInp.split('-');
+                        const {success, message} = validateData(month, day);
+
+                        if(!success) {
+                            console.log(message);
+                            return;
+                        }
+
+                        this.date = createDate(year, month, day);
+                    }
+                },
+                'DDMMYYYY': () => {
+                    if (this.date) {
+                        const {year, month, day} = getDateValues();
+                        return `${day}${month}${year}`;
+                    } else {
+                        const day = this.dateInp.slice(0, 2);
+                        const month = this.dateInp.slice(2, 4);
+                        const year = this.dateInp.slice(4);
+                        const {success, message} = validateData(month, day);
+                        if (!success) {
+                            console.log(message);
+                            return
+                        }
+
+                        this.date = createDate(year, month, day);
+                    }
+                },
+                'Month D, Yr': () => {
+                    if (this.date) {
+                        let year = this.date.getFullYear();
+                        let month = this.date.getMonth();
+                        let day = this.date.getDate();
+
+                        month = this.#monthNames[month];
+                        if (day < 10) {
+                            day = `0${day}`
+                        }
+
+                        return `${month} ${day}, ${year}`;
+
+                    } else {
+                        const [rest, year] = this.dateInp.split(', ');
+                        const [month, day] = rest.split(' ');
+                        let monthIndex = undefined
+
+                        if (this.#monthNames.includes(month)) {
+                            monthIndex = this.#monthNames.indexOf(month) + 1;
+                        }
+
+                        const {success, message} = validateData(monthIndex, day);
+
+                        if (!success) {
+                            console.log(message);
+                            return;
+                        }
+
+                        this.date = createDate(year, monthIndex, day);
+                    }
+                }
+            }
+
+            this.formatInput = function() {
+                const inpType = typeof(this.dateInp);
+            
+                if(inpType != 'string' && inpType != 'number') {
+                    console.log('Incorect date type please use valid date.');
+                    return;
+                } else {
+                    if (inpType === 'string') {
+                        if(dateInp.length < 8) {
+                            console.log('Incorect date length please use valid date.');
+                            return
+                        }
+
+                        if (this.dateFormarts.hasOwnProperty(this.inpFormat)) {
+                            this.dateFormarts[this.inpFormat]();
+                        } else {
+                            console.log(`Incorect format type: Please check valid format types [${Object.keys(dateFormarts)}]`)
+                        }
+                    } else if(inpType === 'number') {
+                        this.date = new Date(this.dateInp);
+                    }
+                }
+
+                
+            }
+            
+            this.formatInput();
+        }
+
+
+        format(outputFormat) {
+            if(this.dateFormarts.hasOwnProperty(outputFormat)) {
+                return this.dateFormarts[outputFormat]();
+            } else {
+                console.log(`Incorect format type: Please check valid format types [${Object.keys(dateFormarts)}]`)
+            }
+        }
+
+        fromNow() {
+            const oldDate = this.date
+            const currentDate = new Date();
+            const oneDay = 1000 * 60 * 60 * 24;
+            const oneHour = 1000 * 60 * 60
+
+            let resultInDays = Math.round((currentDate.getTime() - oldDate.getTime()) / oneDay);
+
+            if (resultInDays >= 365) {
+                const years = currentDate.getFullYear() - oldDate.getFullYear();
+                console.log(`${years} year${years > 1 ? 's' : ''} ago`);
+            } else if (resultInDays >= 31) {
+                const months = currentDate.getMonth() - oldDate.getMonth();
+                console.log(`${months} month${months > 1 ? 's' : ''} ago`);
+            } else if (resultInDays <= 31) {
+                console.log(`${resultInDays} day${resultInDays > 1 ? 's' : ''} ago`)
+            }
+        }
+    }
+
+    const test = new DateParser('01102024', 'DDMMYYYY');
+    const output = test.format('Month D, Yr');
+    console.log(output);
+    test.fromNow()
+}
+
+
+task04_dateFormatter()
 
 
