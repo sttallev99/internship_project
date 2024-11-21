341c482a1f3a0ca9a77d37ac1411fb0375f1bcfb
Георги Сталев <sttallev99@gmail.com>
2024-11-21 18:21:36 +0200

    Add posts persisting

--- topic_four/reddit/src/index.tsx
+++ topic_four/reddit/src/index.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from 'store';
+import { persistor, store } from 'store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- topic_four/reddit/src/store.ts
+++ topic_four/reddit/src/store.ts
@@ -1,13 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import PostsReducer from './reducers/postsSlice';
+import localStorage from "redux-persist/lib/storage";
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+
+const postsPersistConfig = {
+    key: "posts",
+    storage: localStorage,
+}
+
+const persistedReducer = persistReducer(postsPersistConfig, PostsReducer);
 
 export const store = configureStore({
     reducer: {
-        posts: PostsReducer
-    }
+        posts: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            }
+        })
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
